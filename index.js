17e3ee66d8f6343045621a05761487ffa7738cfa
Matthijs Logemann <matthijs.logemann@xs4all.nl>
2016-01-24 12:17:57 +0100

    Fixes issue #3

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ ThinkingCleaner.prototype = {
 			} else {
 				var tcObj = JSON.parse(response.text);
 
-				callback(null, tcObj.status.cleaning);
+				callback(null, tcObj.status.cleaning === "1");
 			}
 		});
 	},
